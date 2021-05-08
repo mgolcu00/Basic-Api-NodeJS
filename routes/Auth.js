3278409c84b15b2eb31a34687c9deff6a8b1589e
mgolcu00 <mgolcu00@gmail.com>
2021-05-08 23:53:14 +0300

    add register func

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User')
 
+// Register 
+router.post('/register', async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
 module.exports = router
