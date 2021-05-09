d4208554b907962f87e8f127006e7ceba588782e
mgolcu00 <mgolcu00@gmail.com>
2021-05-09 16:45:45 +0300

    Password Hashing added.

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,30 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const Joi = require('joi') // for validation
-const User = require('../models/User')
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const { registerValidation, loginValidation } = require('../validation')
 
-const validationSchema = Joi.object({
-    name: Joi.string().min(3).required(),
-    email: Joi.string().min(3).required().email(),
-    password: Joi.string().min(6).required()
-});
-
-// Register 
 router.post('/register', async (req, res) => {
-    // validation sending
-    const {error} = validationSchema.validate(req.body);
-    res.send(error.details[0].message);
 
-    //   const user = new User({
-    //     name: req.body.name,
-    //     email: req.body.email,
-    //     password: req.body.password
-    // });
-    // try {
-    //     const savedUser = await user.save();
-    //     res.send(savedUser);
-    // } catch (err) {
-    //     res.status(400).send(err);
-    // }
+    // validation 
+    const { error } = registerValidation(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
+
+    // Checking if the user already exist
+    const emailExist = await User.findOne({ email: req.body.email })
+    if (emailExist) return res.status(400).send("Email already exist.");
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    // Creating new User
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+    });
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser);
+    } catch (err) {
+        res.status(400).send(err);
+    }
 });
 module.exports = router
